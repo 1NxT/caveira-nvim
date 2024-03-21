09e84526b253b5321d6b850c0a644be96688b61a
1NxT <guirodrigues7863@gmail.com>
2024-03-21 19:43:25 -0300

    refactor: lsp

---
+++ after/plugin/cmp.lua
@@ -0,0 +1,16 @@
+local cmp = require('cmp')
+
+cmp.setup({
+	sources = {
+		{name = 'nvim_lsp'},
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<Tab>'] = cmp.mapping.confirm({select = true}),
+		['<C-Space>'] = cmp.mapping.complete(),
+	}),
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
+})

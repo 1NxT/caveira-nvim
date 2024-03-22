26030b65522564834b56c1dac763c1987a95e94c
1NxT <guirodrigues7863@gmail.com>
2024-03-22 09:58:49 -0300

    fix: cmp_nvim_lsp not found in another directory

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,4 +1,11 @@
-local default_setup = require("after.plugin.cmp_nvim_lsp")
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local default_setup = function(server)
+	require('lspconfig')[server].setup({
+		capabilities = lsp_capabilities,
+	})
+end
+
 require("mason").setup {
     ui = {
         check_outdated_packages_on_open = true,

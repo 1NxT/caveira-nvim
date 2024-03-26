dbd25df695934383f5295630b59ee7a3cb02daf6
1NxT <guirodrigues7863@gmail.com>
2024-03-25 21:26:05 -0300

    feat: added <CR>(Enter) to autocomplete

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -6,6 +6,7 @@ cmp.setup({
 	},
 	mapping = cmp.mapping.preset.insert({
 		['<Tab>'] = cmp.mapping.confirm({select = true}),
+		['<CR>'] = cmp.mapping.confirm({select = true}),
 		['<C-Space>'] = cmp.mapping.complete(),
 	}),
 	snippet = {
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/epta/.cache/nvim/packer_hererocks/2.1.1692716794/share/lua/5.1/?.lua;/home/epta/.cache/nvim/packer_hererocks/2.1.1692716794/share/lua/5.1/?/init.lua;/home/epta/.cache/nvim/packer_hererocks/2.1.1692716794/lib/luarocks/rocks-5.1/?.lua;/home/epta/.cache/nvim/packer_hererocks/2.1.1692716794/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/epta/.cache/nvim/packer_hererocks/2.1.1692716794/lib/lua/5.1/?.so"
+local package_path_str = "/home/nxt/.cache/nvim/packer_hererocks/2.1.1692716794/share/lua/5.1/?.lua;/home/nxt/.cache/nvim/packer_hererocks/2.1.1692716794/share/lua/5.1/?/init.lua;/home/nxt/.cache/nvim/packer_hererocks/2.1.1692716794/lib/luarocks/rocks-5.1/?.lua;/home/nxt/.cache/nvim/packer_hererocks/2.1.1692716794/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/nxt/.cache/nvim/packer_hererocks/2.1.1692716794/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,83 +76,83 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/home/epta/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/home/nxt/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   catppuccin = {
     loaded = true,
-    path = "/home/epta/.local/share/nvim/site/pack/packer/start/catppuccin",
+    path = "/home/nxt/.local/share/nvim/site/pack/packer/start/catppuccin",
     url = "https://github.com/catppuccin/nvim"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/epta/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/home/nxt/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/home/epta/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    path = "/home/nxt/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/home/epta/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/home/nxt/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["neo-tree.nvim"] = {
     loaded = true,
-    path = "/home/epta/.local/share/nvim/site/pack/packer/start/neo-tree.nvim",
+    path = "/home/nxt/.local/share/nvim/site/pack/packer/start/neo-tree.nvim",
     url = "https://github.com/nvim-neo-tree/neo-tree.nvim"
   },
   ["nui.nvim"] = {
     loaded = true,
-    path = "/home/epta/.local/share/nvim/site/pack/packer/start/nui.nvim",
+    path = "/home/nxt/.local/share/nvim/site/pack/packer/start/nui.nvim",
     url = "https://github.com/MunifTanjim/nui.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/epta/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/home/nxt/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/epta/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/home/nxt/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/epta/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/home/nxt/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-treesitter-refactor"] = {
     loaded = true,
-    path = "/home/epta/.local/share/nvim/site/pack/packer/start/nvim-treesitter-refactor",
+    path = "/home/nxt/.local/share/nvim/site/pack/packer/start/nvim-treesitter-refactor",
     url = "https://github.com/nvim-treesitter/nvim-treesitter-refactor"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/home/epta/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/home/nxt/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/nvim-tree/nvim-web-devicons"
   },
   ["nvim-window-picker"] = {
     config = { "\27LJ\2\nç\1\0\0\6\0\f\0\0156\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\n\0005\3\3\0005\4\5\0005\5\4\0=\5\6\0045\5\a\0=\5\b\4=\4\t\3=\3\v\2B\0\2\1K\0\1\0\17filter_rules\1\0\0\abo\fbuftype\1\3\0\0\rterminal\rquickfix\rfiletype\1\0\0\1\4\0\0\rneo-tree\19neo-tree-popup\vnotify\1\0\2\19autoselect_one\2\24include_current_win\1\nsetup\18window-picker\frequire\0" },
     loaded = true,
-    path = "/home/epta/.local/share/nvim/site/pack/packer/start/nvim-window-picker",
+    path = "/home/nxt/.local/share/nvim/site/pack/packer/start/nvim-window-picker",
     url = "https://github.com/s1n7ax/nvim-window-picker"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/epta/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/nxt/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/epta/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/nxt/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/epta/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/home/nxt/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   }
 }

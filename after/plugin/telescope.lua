e74919ea90859934de82e383a7311145209af44c
1NxT <guirodrigues7863@gmail.com>
2024-03-26 20:36:08 -0300

    refactor: ff -> findfiles, fg -> findgit

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -7,8 +7,8 @@ require('telescope').setup{
 }
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff',  builtin.git_files, {})
-vim.keymap.set('n', '<leader>fa',  builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', function() 
+vim.keymap.set('n', '<leader>ff',  builtin.find_files, {})
+vim.keymap.set('n', '<leader>fa',  builtin.git_files, {})
+vim.keymap.set('n', '<leader>fg', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
